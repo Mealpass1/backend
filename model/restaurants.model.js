7f521585f9833481058225eb2902a05b1deb452e
Saad <61246755+Byiringiro-saad@users.noreply.github.com>
2021-12-27 11:49:01 +0200

    Update restaurants.model.js

--- model/restaurants.model.js
+++ model/restaurants.model.js
@@ -1,41 +1,41 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const restaurantsSchema = new Schema({
-    Business_name: {
-        type: String,
-        required: true
-    },
-    Email: {
-        type: String,
-        required: true,
-    },
-    Username: {
-        type: String,
-        required: true
-    },
-    Password: {
-        type: String,
-        required: true,
-    },
-    Office_address: {
-        type: String,
-        required: true
-    },
-    role: {
-        type: String,
-        default: "admin",
-        required: false
-    },
-    imgUrl: {
-        type: String,
-        required: false,
-        default: 'https://res.cloudinary.com/dnfeo5ce9/image/upload/v1637226911/default_ula86a.jpg'
-    },
-    restaurant_desc: {
-        type: String,
-        required: false,
-        default: ''
-    }
-})
-module.exports = mongoose.model("Restaurants", restaurantsSchema)
+  business_name: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    default: "admin",
+    required: false,
+  },
+  imgUrl: {
+    type: String,
+    required: false,
+    default:
+      "https://res.cloudinary.com/dnfeo5ce9/image/upload/v1637226911/default_ula86a.jpg",
+  },
+  description: {
+    type: String,
+    required: false,
+  },
+});
+module.exports = mongoose.model("Restaurant", restaurantsSchema);
