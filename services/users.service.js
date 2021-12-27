279bdc5d352fcb9bf86552227f5d35e75f99823b
Saad <61246755+Byiringiro-saad@users.noreply.github.com>
2021-12-27 12:10:50 +0200

    Update users.service.js

--- services/users.service.js
+++ services/users.service.js
@@ -1,66 +1,68 @@
-const mongoose = require("mongoose")
-const Users = require('../')
-const { hashFunction } = require("../utils/hashFunction")
-const { verifyPassword } = require("../utils/verify.password")
+//packages
+const User = require("../model/user.model");
+const { hashFunction } = require("../utils/hashFunction");
+const { verifyPassword } = require("../utils/verify.password");
+
+//services
 module.exports.RegisterUser = async (req) => {
-  const user = new Users()
-  const { fullname, email, username, password } = req
-  const hashedPassword = await hashFunction(password)
+  const user = new User();
+  const { fullname, email, username, password } = req;
+  const hashedPassword = await hashFunction(password);
   return new Promise((resolve, _) => {
-    user.fullname = fullname
-    user.email = email
-    user.username = username
-    user.password = hashedPassword
-    user.save()
-    resolve(user)
-  })
-}
+    user.fullname = fullname;
+    user.email = email;
+    user.username = username;
+    user.password = hashedPassword;
+    user.save();
+    resolve(user);
+  });
+};
 
 module.exports.UserLogin = async (req) => {
-  const { username, password } = req
-  const user = await Users.findOne({ username: username })
-  if (!user) return false
-  if (!verifyPassword(password, user.password)) return false
-  return user
-}
+  const { username, password } = req;
+  const user = await User.findOne({ username: username });
+  if (!user) return false;
+  if (!verifyPassword(password, user.password)) return false;
+  return user;
+};
 
 module.exports.deleteUser = async (req) => {
-  const { id } = req
-  const user = await Users.deleteOne({ _id: id })
+  const { id } = req;
+  const user = await User.deleteOne({ _id: id });
   if (!user) {
-    return false
+    return false;
   }
-  return user
-}
+  return user;
+};
 
 module.exports.getAllUsers = () => {
-  const users = Users.find()
+  const users = User.find();
   return new Promise((resolve, reject) => {
-    resolve(users)
-  })
-}
+    resolve(users);
+  });
+};
 
 module.exports.getUserById = async (req) => {
-  const { id } = req
-  const user = await Users.findOne({ _id: id })
+  const { id } = req;
+  const user = await User.findOne({ _id: id });
   if (user) {
     return new Promise((resolve, _) => {
-      resolve(user)
-    })
+      resolve(user);
+    });
   }
-  return
-}
+  return;
+};
 
 module.exports.updateUser = async (userId, newUser) => {
-  const { fullname, email, username } = newUser
-  const updatedUser = await Users.findByIdAndUpdate(userId, newUser, {
+  const { fullname, email, username } = newUser;
+  const updatedUser = await User.findByIdAndUpdate(userId, newUser, {
     new: true,
     runValidators: true,
     useFindAndModify: false,
-  })
+  });
   if (updatedUser) {
     return new Promise((resolve, _) => {
-      resolve(updatedUser)
-    })
+      resolve(updatedUser);
+    });
   }
-}
+};
