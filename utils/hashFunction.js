d80651106a8eca054d311da1f7179cf8e7a4a789
Saad <61246755+Byiringiro-saad@users.noreply.github.com>
2021-12-27 12:31:38 +0200

    Delete hashFunction.js

--- utils/hashFunction.js
+++
@@ -1,6 +0,0 @@
-const bcrypt = require('bcrypt');
-module.exports.hashFunction=async(val)=>{
-    const salt = await bcrypt.genSalt(10);
-    const finalVal = await bcrypt.hash(val,salt);
-    return finalVal;
-}
