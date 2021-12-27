4fe173c50ea7be8b959be9ac50d2b28553c3d270
Saad <61246755+Byiringiro-saad@users.noreply.github.com>
2021-12-27 13:35:02 +0200

    Update restaurant.exists.js

--- helpers/restaurant.exists.js
+++ helpers/restaurant.exists.js
@@ -1,13 +1,9 @@
-const mongoose = require('mongoose');
-const Restaurants = require('../model/restaurants.model');
+const Restaurants = require("../model/restaurants.model");
 
-
-module.exports.checkRestaurantExistence = async(email,businessName,username)=>{
- const usernameExists = await Restaurants.findOne({Username:username});
- const emailExists = await Restaurants.findOne({Email:email});
- const businessnameExists = await Restaurants.findOne({Business_name:businessName});
- if(usernameExists || emailExists || businessnameExists ){
-     return true;
- }
- return false;
-}
+module.exports.checkRestaurantExistence = async (email) => {
+  const emailExists = await Restaurants.findOne({ email: email });
+  if (emailExists) {
+    return true;
+  }
+  return false;
+};
