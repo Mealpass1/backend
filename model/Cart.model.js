c5b3eee136f593f940d7e20cad2865afed89e669
Saad <61246755+Byiringiro-saad@users.noreply.github.com>
2021-12-27 11:23:52 +0200

    renewing the cart model

--- model/Cart.model.js
+++ model/Cart.model.js
@@ -1,67 +1,55 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-
 const cartSchema = new Schema({
-    dishId:{
-        type: String,
-        required: true
+  dishId: {
+    type: mongoose.Types.ObjectId,
+    ref: "dish",
+  },
+  userId: {
+    type: String,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  total: {
+    type: Number,
+    default: 0,
+  },
+  imgUrl: {
+    type: String,
+    required: true,
+  },
+  timeOfMeal: {
+    type: String,
+    required: false,
+  },
+  createdAt: Date,
+  daysInWeek: [
+    {
+      type: String,
+      enum: [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday",
+      ],
+      required: false,
     },
-    dishName:{
-        type: String,
-        required: true
-    },
-    userId:{
-        type: String,
-        required: true
-    },
-    quantity:{
-        type: Number,
-        required: true,
-        min:[1,'quantity should be positive']
-    },
-    price:{
-        type: Number,
-        required: true
-    },
-    total:{
-        type: Number,
-        default: 0
-    },
-    imgUrl:{
-        type: String,
-        required: true
-    },
-    timeOfMeal:{
-        type: String,
-        required: false
-    },
-    daysInWeek:[{
-        type: String,
-        enum: ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'],
-        required: false
-    }],
-    NumberOfRepetition:{
-        type: String,
-        required: false
-    },
-    additionalTopping:[
-        {
-        toppingName:{
-            type: String,
-            required: false,
-        },
-        toppingPrice:{
-            type: Number,
-            required: false,
-        }
-    }
-    ]
+  ],
+  NumberOfRepetition: {
+    type: String,
+    required: false,
+  },
+});
 
-}, {timestamps: true}
-);
-
-
-module.exports = mongoose.model('Cart',cartSchema);
-
-
+module.exports = mongoose.model("cart", cartSchema);
