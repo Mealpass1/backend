93c920f39274f5dd703c96d0e44b7e1d2c776240
Saad <61246755+Byiringiro-saad@users.noreply.github.com>
2021-12-27 11:48:53 +0200

    Update image.model.js

--- model/image.model.js
+++ model/image.model.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
+
 const Schema = mongoose.Schema;
+
 const ImageSchema = new Schema({
-    url: { type: String },
-    description: { type: String },
+  url: { type: String },
+  description: { type: String },
 });
 
-module.exports = mongoose.model('Image',ImageSchema);
+module.exports = mongoose.model("Image", ImageSchema);
