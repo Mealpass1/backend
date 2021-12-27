d74a6d90a8f1d9d7d920eacf0ebc54a993839a85
Saad <61246755+Byiringiro-saad@users.noreply.github.com>
2021-12-27 13:34:41 +0200

    Update restaurants.controller.js

--- controllers/restaurants.controller.js
+++ controllers/restaurants.controller.js
@@ -1,3 +1,8 @@
+//packages
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+
+//services
 const {
   createNewRestaurant,
   getAllRestaurants,
@@ -5,107 +10,112 @@ const {
   deleteRestaurant,
   updateRestaurant,
   LoginRestaurant,
-  restaurantProfile,
 } = require("../services/restaurants.service");
-const jwt = require("jsonwebtoken");
+
+//helpers
 const { checkRestaurantExistence } = require("../helpers/restaurant.exists");
 
+//configure dotenv
+dotenv.config();
+
+//controllers
 module.exports.createNewRestaurantController = async (req, res) => {
-  if (
-    (await checkRestaurantExistence(
-      req.body.Email,
-      req.body.Business_name,
-      req.body.Username
-    )) === true
-  ) {
-    return res.status(400).send({
+  if ((await checkRestaurantExistence(req.body.email)) === true) {
+    return res.json({
+      status: "error",
       message: "restaurant exists",
     });
+  } else {
+    const restraunt = await createNewRestaurant(req.body);
+    if (restraunt) {
+      return res.json({
+        status: "success",
+        message: "restaurant created",
+      });
+    } else {
+      return res.json({
+        status: "error",
+        message: "Failed to create restaurant",
+      });
+    }
   }
-  const results = await createNewRestaurant(req.body);
-  if (results) {
-    return res.status(200).json({
-      status: true,
-      msg: "restaurant successfully saved",
-    });
-  }
-  return res.status(400).json({
-    status: false,
-    msg: "Failed to create restaurant",
-  });
 };
 
 module.exports.getAllRestaurantsController = async (_, res) => {
   const restraunts = await getAllRestaurants();
   if (restraunts) {
-    return res.status(200).json({
-      message: "Restraunts List: ",
+    return res.json({
+      status: "success",
+      message: "all restuarants",
       data: restraunts,
     });
+  } else {
+    return res.json({
+      status: "error",
+      message: "no restaurants found",
+    });
   }
-  return res.status(404).send("No Restaurants Found");
 };
 
 module.exports.getRestrauntByIdController = async (req, res) => {
   const restraunt = await getRestrauntById(req.params);
   if (restraunt) {
-    return res.status(200).json({
-      message: "Result: ",
+    return res.json({
+      status: "success",
+      message: "restaurant",
       data: restraunt,
     });
+  } else {
+    return res.json({
+      status: "error",
+      message: "no restaurant found",
+    });
   }
-  return res.status(404).send("No Corresponding Restraunt Found");
 };
 
 module.exports.deleteRestrauntController = async (req, res) => {
-  const results = await deleteRestaurant(req.params);
-  if (results) {
-    return res.status(200).json({
-      message: "Restaurant deleted successfully",
-      data: results,
+  const response = await deleteRestaurant(req.params);
+  if (response) {
+    return res``.json({
+      status: "success",
+      message: "restaurant deleted",
+    });
+  } else {
+    return res.json({
+      status: "error",
+      message: "no restaurant found",
     });
   }
-  return res.status(404).send("No Corresponding Restraunt Found");
 };
 
 module.exports.updateRestaurantController = async (req, res) => {
-  const updatedRestraunt = await updateRestaurant(req.params.id, req.body);
-  if (updatedRestraunt) {
-    return res.status(200).send("Restraunt Updated Successfully");
+  const response = await updateRestaurant(req.params.id, req.body);
+  if (response) {
+    return res.json({
+      status: "success",
+      message: "restaurant updated",
+    });
+  } else {
+    return res.json({
+      status: "error",
+      message: "no restaurant found",
+    });
   }
-  return res.status(400).send("Failed To Update The Restraunt");
 };
 
 module.exports.LoginRestaurantController = async (req, res) => {
-  const loggedInRestaurant = await LoginRestaurant(req.body);
-  if (loggedInRestaurant) {
-    const id = loggedInRestaurant._id;
-    let payload = { id };
-    let accessToken = jwt.sign(payload, "swsh23hjddnns", {
-      algorithm: "HS256",
-      expiresIn: 86400,
+  const response = await LoginRestaurant(req.body);
+  if (response) {
+    let token = jwt.sign(response._id, `${process.env.SECRET}`);
+    return res.json({
+      status: "success",
+      message: "restaurant logged in",
+      data: token,
     });
-    res.cookie("jwt", accessToken, { secure: true, httpOnly: true });
-    res.status(200).send({
-      token: accessToken,
-      message: "Login successful",
+  } else {
+    return res.json({
+      status: "error",
+      message: "invalid email or password",
     });
   }
-};
-
-module.exports.restaurantProfileController = async (req, res) => {
-  const profile = await restaurantProfile(req.restaurant.restrauntId, req.body)
-    .then((response) => {
-      return res.status(200).json({
-        message: "Restaurant profile",
-        status: "ok",
-        data: profile,
-      });
-    })
-    .catch((err) => {
-      return res.status(500).json({
-        message: "server error",
-        status: "Failed",
-      });
-    });
 };
