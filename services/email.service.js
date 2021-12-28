d961076763ba7f0eec614d61153b09eacbc40ee4
Saad <61246755+Byiringiro-saad@users.noreply.github.com>
2021-12-28 19:08:12 +0200

    Create email.service.js

---
+++ services/email.service.js
@@ -0,0 +1,38 @@
+//packages
+const nodemailer = require("nodemailer");
+
+const dotenv = require("dotenv").config();
+
+const transporter = nodemailer.createTransport({
+  port: 465,
+  host: "smtp.gmail.com",
+  auth: {
+    user: "sajuengine@gmail.com",
+    pass: process.env.PASS,
+  },
+  secure: true,
+});
+
+const sendEmail = (email, names) => {
+  const maildata = {
+    from: "sajuengine@gmail.com",
+    to: `${email}`,
+    subject: "Welcome✌️✌️.",
+    html: `
+        <h2>Hello ${names}</h2><br>
+        <b>Thanks alot for trying out our application</b><br>
+        <b>Hope you find it amazing</b>
+    `,
+  };
+
+  transporter.sendMail(maildata, (err, info) => {
+    if (err) {
+      console.log("error while sending an email");
+      console.log(err.message);
+    } else {
+      console.log("email sent successfully");
+    }
+  });
+};
+
+export default sendEmail;
