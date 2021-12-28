b7ad84b146e68fbce0e36c76810cb2e8d6cec8dd
Saad <61246755+Byiringiro-saad@users.noreply.github.com>
2021-12-28 17:47:16 +0200

    Create diner.auth.js

---
+++ middlewares/diner.auth.js
@@ -0,0 +1,40 @@
+const jwt = require("jsonwebtoken");
+
+const dotenv = require("dotenv").config();
+
+const Diner = require("../models/diner.model");
+
+const auth = async (req, res, next) => {
+  if (!req.headers.auth) {
+    return res.json({
+      status: "error",
+      message: "No authorization specified",
+    });
+  } else {
+    jwt.verify(
+      req.headers.auth,
+      `${process.env.SECRET}`,
+      async (err, decoded) => {
+        if (err) {
+          return res.json({
+            status: "error",
+            message: "Not Authorised to this service",
+          });
+        } else {
+          await Diner.findOne({ _id: decoded.id }).then((diner) => {
+            if (!diner) {
+              return res.json({
+                status: "error",
+                message: "account doesn't exists",
+              });
+            } else {
+              next();
+            }
+          });
+        }
+      }
+    );
+  }
+};
+
+module.exports = auth;
