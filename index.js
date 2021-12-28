2377d0be56ecb99b486d8248400574e3f46fafc3
Saad <61246755+Byiringiro-saad@users.noreply.github.com>
2021-12-28 15:37:34 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+//packages
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv").config();
+
+//files
+const dinerRoutes = require("./routes/diner.routes");
+
+//initialize app
+const app = express();
+
+//some configurations
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//base endpoints
+app.use("/diner", dinerRoutes);
+
+//start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
