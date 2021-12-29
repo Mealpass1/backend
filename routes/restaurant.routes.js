f13abf6c6586bef4079eab7d1cacd49f208229a6
Saad <61246755+Byiringiro-saad@users.noreply.github.com>
2021-12-29 18:37:21 +0200

    Update restaurant.routes.js

--- routes/restaurant.routes.js
+++ routes/restaurant.routes.js
@@ -1,9 +1,19 @@
 const express = require("express");
 
-const { signup } = require("../controllers/restaurant.controllers");
+const {
+  signup,
+  login,
+  logout,
+  allRestaurants,
+} = require("../controllers/restaurant.controllers");
+
+const authMiddleware = require("../middlewares/restaurant.auth");
 
 const router = express.Router();
 
 router.post("/signup", signup);
+router.post("/login", login);
+router.put("/logout/:id", authMiddleware, logout);
+router.get("/", allRestaurants);
 
 module.exports = router;
