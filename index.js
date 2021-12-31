8b71c6b0bee77d1628f12afdee91a7e8b57c51be
Saad <61246755+Byiringiro-saad@users.noreply.github.com>
2021-12-31 22:07:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,54 @@
 //packages
 const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const dotenv = require("dotenv").config();
+const cluster = require("cluster");
+const totalCPUs = require("os").cpus().length;
 
-//files
-const dinerRoutes = require("./routes/diner.routes");
-const cartRoutes = require("./routes/cart.routes");
-const restaurantRoutes = require("./routes/restaurant.routes");
-const dishRoutes = require("./routes/dish.routes");
-const orderRoutes = require("./routes/order.routes");
-const menuRoutes = require("./routes/menu.routes");
+if (cluster.isPrimary) {
+  for (let i = 0; i < totalCPUs; i++) {
+    cluster.fork();
+  }
 
-//initialize app
-const app = express();
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+    // console.log("Let's fork another worker!");
+    // cluster.fork();
+  });
+} else {
+  //packages
+  const cors = require("cors");
+  const bodyParser = require("body-parser");
+  const dotenv = require("dotenv").config();
 
-//databse
-require("./services/db.service");
+  //files
+  const dinerRoutes = require("./routes/diner.routes");
+  const cartRoutes = require("./routes/cart.routes");
+  const restaurantRoutes = require("./routes/restaurant.routes");
+  const dishRoutes = require("./routes/dish.routes");
+  const orderRoutes = require("./routes/order.routes");
+  const menuRoutes = require("./routes/menu.routes");
 
-//some configurations
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+  //initialize app
+  const app = express();
 
-//base endpoints
-app.use("/diner", dinerRoutes);
-app.use("/cart", cartRoutes);
-app.use("/restaurant", restaurantRoutes);
-app.use("/dish", dishRoutes);
-app.use("/order", orderRoutes);
-app.use("/menu", menuRoutes);
+  //databse
+  require("./services/db.service");
 
-//start the server
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  //some configurations
+  app.use(cors());
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  //base endpoints
+  app.use("/diner", dinerRoutes);
+  app.use("/cart", cartRoutes);
+  app.use("/restaurant", restaurantRoutes);
+  app.use("/dish", dishRoutes);
+  app.use("/order", orderRoutes);
+  app.use("/menu", menuRoutes);
+
+  //start the server
+  const PORT = process.env.PORT || 8080;
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
