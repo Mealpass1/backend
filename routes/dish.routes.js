659445dcd74d7269e83ee0ffbfae0400118ed8a1
Saad <61246755+Byiringiro-saad@users.noreply.github.com>
2021-12-31 12:52:45 +0200

    Update dish.routes.js

--- routes/dish.routes.js
+++ routes/dish.routes.js
@@ -1,5 +1,19 @@
 const express = require("express");
 
+const {
+  createDish,
+  allDishes,
+  oneDish,
+} = require("../controllers/dish.controllers");
+
+const authMiddleware = require("../middlewares/restaurant.auth");
+
+const upload = require("../services/imageUpload.service");
+
 const router = express.Router();
+
+router.post("/add", authMiddleware, upload.single("picture"), createDish);
+router.get("/", allDishes);
+router.get("/:id", oneDish);
 
 module.exports = router;
