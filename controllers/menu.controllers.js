85fb3044775f6c0d7b9193e731df5dd65b0b1b28
Saad <61246755+Byiringiro-saad@users.noreply.github.com>
2021-12-31 21:47:37 +0200

    Create menu.controllers.js

---
+++ controllers/menu.controllers.js
@@ -0,0 +1,39 @@
+const Menu = require("../models/menu.model");
+
+exports.getMenu = async (req, res) => {
+  await Menu.find({ diner: req.diner._id })
+    .exec()
+    .populate("order", "dish")
+    .then((menu) => {
+      return res.json({
+        status: "success",
+        message: "Menu",
+        data: menu,
+      });
+    })
+    .catch((err) => {
+      return res.json({
+        status: "error",
+        message: err.message,
+      });
+    });
+};
+
+exports.getOrder = async (req, res) => {
+  await Menu.findById(req.params.id)
+    .exec()
+    .populate("order", "dish")
+    .then((order) => {
+      return res.json({
+        status: "success",
+        message: "Menu item",
+        data: order,
+      });
+    })
+    .catch((err) => {
+      return res.json({
+        status: "error",
+        message: err.message,
+      });
+    });
+};
