ef54a40d34d56b2a5c496bff2d6608b2ab834d83
Saad <61246755+Byiringiro-saad@users.noreply.github.com>
2022-01-01 19:59:41 +0200

    Create cloudinary.service.js

---
+++ services/cloudinary.service.js
@@ -0,0 +1,26 @@
+const cloudinary = require("cloudinary").v2;
+
+const dotenv = require("dotenv").config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
+
+exports.uploads = (file, folder) => {
+  return new Promise((resolve, reject) => {
+    cloudinary.uploader.upload(
+      file,
+      (result) => {
+        resolve({
+          url: result.url,
+        });
+      },
+      {
+        resource_type: "auto",
+        folder: folder,
+      }
+    );
+  });
+};
