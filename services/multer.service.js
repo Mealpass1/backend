ccf7310b372b2dd8e15797c3db0ad967f8f1660c
Saad <61246755+Byiringiro-saad@users.noreply.github.com>
2022-01-06 17:19:38 +0200

    Update multer.service.js

--- services/multer.service.js
+++ services/multer.service.js
@@ -2,16 +2,20 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "./mealpass");
+    cb(null, "mealpass");
   },
   filename: (req, file, cb) => {
-    cb(null, new Date().toString() + "_" + file.originalname);
+    cb(null, Date.now() + "-" + file.originalname);
   },
 });
 
+const fileFilter = (req, file, cb) => {
+  cd(null, true);
+};
+
 const upload = multer({
-  storage: "mealpass",
-  limits: { fieldSize: 10 * 1024 * 1024 },
+  storage: storage,
+  fileFilter: fileFilter,
 });
 
 module.exports = upload;
