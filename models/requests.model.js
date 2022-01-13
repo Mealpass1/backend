a601c8346351c5738390f2aa91a23d5547c2b16d
Saad <61246755+Byiringiro-saad@users.noreply.github.com>
2022-01-13 20:31:17 +0200

    creating request model

---
+++ models/requests.model.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const requestSchema = new Schema({
+  diner: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+  },
+  dish: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+  },
+  order: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+  },
+  restaurant: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["pending", "completed", "failed"],
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Requests", requestSchema);
