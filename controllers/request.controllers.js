242293880e842f4469e2c34ec083ea2d89586bb7
Saad <61246755+Byiringiro-saad@users.noreply.github.com>
2022-01-17 12:01:21 +0200

    create request endpoint

--- controllers/request.controllers.js
+++ controllers/request.controllers.js
@@ -1,3 +1,33 @@
+const Dish = require("../models/dish.model");
+const Menu = require("../models/menu.model");
+const Order = require("../models/order.model");
+const Request = require("../models/request.model");
+
 exports.create = async (req, res) => {
-  res.send("Hii");
+  const data = {
+    diner: req.body.diner,
+    dish: req.body.dish,
+    order: req.body.order,
+    restaurant: req.body.restaurant,
+    status: "pending",
+    quantity: 1,
+  };
+
+  await Dish.findOneById(data.dish).then(async (dish) => {
+    if (!dish) {
+      return res.json({
+        status: "error",
+        message: "dish doesn't exist",
+      });
+    } else {
+      await Order.findById(data.order).then(async (order) => {
+        if (!order) {
+          return res.json({
+            status: "error",
+            message: "order doesn't exist",
+          });
+        }
+      });
+    }
+  });
 };
