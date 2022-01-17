728709371cc2bec4b19ff6587f41ead797bce4d5
Saad <61246755+Byiringiro-saad@users.noreply.github.com>
2022-01-17 20:44:36 +0200

    Update request.model.js

--- models/request.model.js
+++ models/request.model.js
@@ -5,18 +5,22 @@ const Schema = mongoose.Schema;
 const requestSchema = new Schema({
   diner: {
     type: mongoose.Types.ObjectId,
+    ref: "Diner",
     required: true,
   },
   dish: {
     type: mongoose.Types.ObjectId,
+    ref: "Dish",
     required: true,
   },
   order: {
     type: mongoose.Types.ObjectId,
+    ref: "Order",
     required: true,
   },
   restaurant: {
     type: mongoose.Types.ObjectId,
+    ref: "Restaurant",
     required: true,
   },
   status: {
