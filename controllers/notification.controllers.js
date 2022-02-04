45213398ac8481b900917d57f7e1fd37497203c3
Saad <61246755+Byiringiro-saad@users.noreply.github.com>
2022-02-04 06:04:42 +0200

    Update notification.controllers.js

--- controllers/notification.controllers.js
+++ controllers/notification.controllers.js
@@ -5,33 +5,22 @@ exports.subscribe = async (req, res) => {
     subscription: req.body.subscription,
   };
 
-  const phone = req.diner.pushSubscription.filter((phone) => {
-    return phone == data.subscription;
-  });
-
-  if (phone.length == 0) {
-    await Diner.findOneAndUpdate(
-      { _id: req.diner._id },
-      {
-        $push: { pushSubscription: data.subscription },
-      }
-    )
-      .then((response) => {
-        return res.json({
-          status: "success",
-          message: "phone registered for notifications",
-        });
-      })
-      .catch((err) => {
-        return res.json({
-          status: "error",
-          message: err.message,
-        });
+  await Diner.findOneAndUpdate(
+    { _id: req.diner._id },
+    {
+      pushSubscription: data.subscription,
+    }
+  )
+    .then((response) => {
+      return res.json({
+        status: "success",
+        message: "phone registered for notifications",
       });
-  } else {
-    return res.json({
-      status: "success",
-      message: "phone already registered for notifications",
+    })
+    .catch((err) => {
+      return res.json({
+        status: "error",
+        message: err.message,
+      });
     });
-  }
 };
