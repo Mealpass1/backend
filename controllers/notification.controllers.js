ea13172026d57c6ac4aa17ca23d85a2855d3a7a3
Saad <61246755+Byiringiro-saad@users.noreply.github.com>
2022-02-07 19:21:35 +0200

    Update notification.controllers.js

--- controllers/notification.controllers.js
+++ controllers/notification.controllers.js
@@ -52,7 +52,7 @@ exports.restaurantSubscribe = async (req, res) => {
     });
 };
 
-exports.notifications = async (req, res) => {
+exports.dinerNotifications = async (req, res) => {
   await Notification.find({ diner: req.diner._id })
     .then((response) => {
       return res.json({
@@ -69,12 +69,53 @@ exports.notifications = async (req, res) => {
     });
 };
 
-exports.see = async (req, res) => {
+exports.restaurantNotifications = async (req, res) => {
+  await Notification.find({ restaurant: req.restaurant._id })
+    .then((response) => {
+      return res.json({
+        status: "success",
+        message: "all notifications",
+        data: response,
+      });
+    })
+    .catch((error) => {
+      return res.json({
+        status: "error",
+        message: error.message,
+      });
+    });
+};
+
+exports.dinerSee = async (req, res) => {
   const data = {
     notification: req.body.notification,
   };
   await Notification.findOneAndUpdate(
     { diner: req.diner._id, _id: data.notification },
+    {
+      seen: true,
+    }
+  )
+    .then((response) => {
+      return res.json({
+        status: "success",
+        message: "seen",
+      });
+    })
+    .catch((error) => {
+      return res.json({
+        status: "error",
+        message: error.message,
+      });
+    });
+};
+
+exports.restaurantSee = async (req, res) => {
+  const data = {
+    notification: req.body.notification,
+  };
+  await Notification.findOneAndUpdate(
+    { restaurant: req.restaurant._id, _id: data.notification },
     {
       seen: true,
     }
