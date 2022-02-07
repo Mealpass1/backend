7389d5ebfe9c5446277c7bf273d8def55ef2cebc
Saad <61246755+Byiringiro-saad@users.noreply.github.com>
2022-02-07 14:52:44 +0200

    add some new restaurant routes

--- routes/notification.routes.js
+++ routes/notification.routes.js
@@ -1,16 +1,20 @@
 const express = require("express");
 
 const {
-  subscribe,
+  dinerSubscribe,
+  restaurantSubscribe,
   notifications,
   see,
 } = require("../controllers/notification.controllers");
+
+const restaurantAuth = require("../middlewares/restaurant.auth");
 const dinerAuth = require("../middlewares/diner.auth");
 
 const router = express.Router();
 
 router.get("/", dinerAuth, notifications);
-router.post("/subscribe", dinerAuth, subscribe);
+router.post("/diner/subscribe", dinerAuth, dinerSubscribe);
+router.post("/restaurant/subscribe", restaurantAuth, restaurantSubscribe);
 router.post("/seen", dinerAuth, see);
 
 module.exports = router;
