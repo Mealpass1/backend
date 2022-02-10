2d7012e4e91b595dd0cb6dbed9f35032d35a54cc
Saad <61246755+Byiringiro-saad@users.noreply.github.com>
2022-02-10 09:25:40 +0200

    Update order.controllers.js

--- controllers/order.controllers.js
+++ controllers/order.controllers.js
@@ -23,7 +23,128 @@ exports.create = async (req, res) => {
 
     try {
       await Cart.find({ _id: item.cart })
-        .then(async (cart) => {})
+        .then(async (cart) => {
+          if (cart) {
+            const order = new Order({
+              diner: req.diner._id,
+              restaurant: item.restaurant,
+              dish: item.dish,
+              quantity: cart.quantity,
+              timeOfMeal: cart.timeOfMeal,
+              daysInWeek: cart.daysInWeek,
+              deliveryMode: cart.deliveryMode,
+              repeatsInMonth: cart.repeatsInMonth,
+              mealServing: {
+                used: 0,
+                unused: cart.mealServing,
+              },
+              createdAt: Date.now(),
+            });
+
+            await order
+              .save()
+              .then(async (order) => {
+                const menu = new Menu({
+                  diner: req.diner._id,
+                  order: order._id,
+                  dish: item.dish,
+                  restaurant: item.restaurant,
+                  status: "pending",
+                  createdAt: Date.now(),
+                });
+
+                await menu
+                  .save()
+                  .then(async (menu) => {
+                    await Dish.findOneAndUpdate(
+                      { _id: item.dish },
+                      {
+                        $inc: { "sales.diners": +1 },
+                        $inc: { "sales.money": +cart.subTotal },
+                      }
+                    )
+                      .then(async (response) => {
+                        await Restaurant.findOneAndUpdate(
+                          { _id: item.restaurant },
+                          {
+                            $inc: { revenue: +cart.subTotal },
+                          }
+                        )
+                          .then(async (response) => {
+                            await Diner.findOneAndUpdate(
+                              { _id: req.diner._id },
+                              {
+                                $inc: { purchases: +cart.subTotal },
+                                $pull: { cart: item.cart },
+                              }
+                            )
+                              .then(async (response) => {
+                                await Restaurant.findById(item.restaurant)
+                                  .then((restaurant) => {
+                                    const dinerBody = JSON.stringify({
+                                      title: "Order paid",
+                                      description: `Your Order was sent to ${restaurant.businessName} restaurant`,
+                                      icon: `${process.env.ICON}`,
+                                    });
+
+                                    const restaurantBody = JSON.stringify({
+                                      title: "New Order",
+                                      description: `${req.diner.username} placed a new order`,
+                                      icon: `${process.env.ICON}`,
+                                    });
+
+                                    webPush
+                                      .sendNotification(
+                                        restaurant.pushSubscription,
+                                        restaurantBody
+                                      )
+                                      .catch((err) => {
+                                        throw new Error(
+                                          "restaurant notification not sent"
+                                        );
+                                      });
+
+                                    webPush
+                                      .sendNotification(
+                                        req.diner.pushSubscription,
+                                        dinerBody
+                                      )
+                                      .catch((err) => {
+                                        throw new Error(
+                                          "restaurant notification not sent"
+                                        );
+                                      });
+
+                                    session.commitTransaction();
+                                    session.endSession();
+                                  })
+                                  .catch((error) => {
+                                    throw new Error("restaurant not found");
+                                  });
+                              })
+                              .catch((err) => {
+                                throw new Error("diner not updated");
+                              });
+                          })
+                          .catch((err) => {
+                            throw new Error("restaurant not updated");
+                          });
+                      })
+                      .catch((err) => {
+                        throw new Error("dish not updated");
+                      });
+                  })
+                  .catch((err) => {
+                    throw new Error("menu not created");
+                  });
+              })
+              .catch((err) => {
+                throw new Error("order not saved");
+              });
+          } else {
+            throw new Error("item not found in cart");
+          }
+        })
         .catch((err) => {
           await session.abortTransaction();
           session.endSession();
