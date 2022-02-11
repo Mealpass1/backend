d6662510c5c0f35c70fbba99cb99ba62f85abced
Saad <61246755+Byiringiro-saad@users.noreply.github.com>
2022-02-11 10:47:15 +0200

    Update menu.controllers.js

--- controllers/menu.controllers.js
+++ controllers/menu.controllers.js
@@ -1,6 +1,7 @@
 //libraries
 const webPush = require("web-push");
 const dotenv = require("dotenv").config();
+const mongoose = require("mongoose");
 
 //models
 const Menu = require("../models/menu.model");
@@ -50,136 +51,86 @@ exports.getOrder = async (req, res) => {
 };
 
 exports.shareOrder = async (req, res) => {
-  const data = {
-    person: req.body.person,
-    quantity: req.body.quantity,
-    order: req.body.order,
-    dish: req.body.dish,
-    restaurant: req.body.restaurant,
-  };
+  const session = mongoose.startSession();
+  await session.startTransaction();
 
-  let selected = "";
+  try {
+    const data = {
+      person: req.body.person,
+      quantity: req.body.quantity,
+      order: req.body.order,
+      dish: req.body.dish,
+      restaurant: req.body.restaurant,
+    };
 
-  const email = await Diner.findOne({ email: data.person });
-  const username = await Diner.findOne({ username: data.person });
+    let selected = "";
 
-  if (email) {
-    selected = "email";
-  } else if (username) {
-    selected = "username";
-  } else {
+    const email = await Diner.findOne({ email: data.person });
+    const username = await Diner.findOne({ username: data.person });
+
+    if (email) {
+      selected = "email";
+    } else if (username) {
+      selected = "username";
+    } else {
+      throw new Error("user not found");
+    }
+
+    await Order.findOne({ _id: data.order })
+      .then(async (order) => {
+        if (order.mealServing.unused > data.quantity) {
+          const body = JSON.stringify({
+            title: "Meal Share",
+            description: `${req.diner.username} invites you to share meal`,
+            icon: `${process.env.ICON}`,
+          });
+          webPush
+            .sendNotification(
+              email?.pushSubscription || username?.pushSubscription,
+              body
+            )
+            .then(async (response) => {
+              const notification = new Notification({
+                diner: email?._id || username?._id,
+                title: "Meal share",
+                body: `@${req.diner.username} invited you to share meal`,
+                menu: `${menu._id}`,
+                order: `${order._id}`,
+                dish: `${data.dish}`,
+                createdAt: Date.now(),
+              });
+
+              notification
+                .save()
+                .then(async (response) => {
+                  await session.commitTransaction();
+                  await session.endSession();
+                })
+                .catch((error) => {
+                  throw new Error("notification not saved");
+                });
+            })
+            .catch((err) => {
+              throw new Error("notification not sent");
+            });
+        } else {
+          throw new Error("invalid mealserving quantity");
+        }
+      })
+      .catch((err) => {
+        throw new Error("order not found");
+      });
+  } catch (error) {
+    await session.abortTransaction();
+    await session.endSession();
     return res.json({
       status: "error",
-      message: "user not found",
+      message: error.message,
     });
   }
 
-  await Order.findOne({ _id: data.order })
-    .then(async (order) => {
-      if (order.mealServing.unused > data.quantity) {
-        const menu = new Menu({
-          invite: {
-            from: req.diner.username,
-            quantity: data.quantity,
-          },
-          diner: email?._id || username?._id,
-          order: data.order,
-          dish: data.dish,
-          restaurant: data.restaurant,
-          status: "pending",
-          createdAt: Date.now(),
-        });
-
-        await menu
-          .save()
-          .then((menu) => {
-            const notification = new Notification({
-              diner: email?._id || username?._id,
-              title: "Meal share",
-              body: `@${req.diner.username} invited you to share meal`,
-              menu: `${menu._id}`,
-              order: `${order._id}`,
-              dish: `${data.dish}`,
-              createdAt: Date.now(),
-            });
-
-            notification
-              .save()
-              .then((response) => {
-                const body = JSON.stringify({
-                  title: "Meal Share",
-                  description: `${req.diner.username} invites you to share meal`,
-                  icon: `${process.env.ICON}`,
-                });
-                webPush.sendNotification(
-                  email?.pushSubscription || username?.pushSubscription,
-                  body
-                );
-                Order.findOneAndUpdate(
-                  { _id: data.order },
-                  {
-                    $inc: { "mealServing.used": +data.quantity },
-                    $inc: { "mealServing.unused": -data.quantity },
-                  }
-                )
-                  .then((response) => {
-                    Menu.findOneAndUpdate(
-                      { diner: req.diner._id },
-                      {
-                        shared: true,
-                        $push: {
-                          sharing: {
-                            date: Date.now(),
-                            to: email?.username || username?.username,
-                            quantity: data.quantity,
-                          },
-                        },
-                      }
-                    )
-                      .then((response) => {
-                        return res.json({
-                          status: "success",
-                          message: "Meal shared",
-                        });
-                      })
-                      .catch((error) => {
-                        return res.json({
-                          status: "error",
-                          message: error.message,
-                        });
-                      });
-                  })
-                  .catch((err) => {
-                    return res.json({
-                      status: "error",
-                      message: error.message,
-                    });
-                  });
-              })
-              .catch((error) => {
-                return res.json({
-                  status: "error",
-                  message: error.message,
-                });
-              });
-          })
-          .catch((error) => {
-            return res.json({
-              status: "error",
-              message: error.message,
-            });
-          });
-      } else {
-        return res.json({
-          status: "error",
-          message: "Invalid mealServing quantity",
-        });
-      }
-    })
-    .catch((err) => {
-      return res.json({
-        status: "error",
-        message: "order not found",
-      });
-    });
+  return res.json({
+    status: "succcess",
+    message: "order shared",
+  });
 };
