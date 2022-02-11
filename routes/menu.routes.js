cefc44681425b8a59bac5ffd46e8926eec1986d2
Saad <61246755+Byiringiro-saad@users.noreply.github.com>
2022-02-12 00:16:47 +0200

    Update menu.routes.js

--- routes/menu.routes.js
+++ routes/menu.routes.js
@@ -4,6 +4,8 @@ const {
   getMenu,
   getOrder,
   shareOrder,
+  acceptShare,
+  declineShare,
 } = require("../controllers/menu.controllers.js");
 
 const authMiddleware = require("../middlewares/diner.auth");
@@ -13,5 +15,7 @@ const router = express.Router();
 router.get("/", authMiddleware, getMenu);
 router.get("/:id", authMiddleware, getOrder);
 router.post("/share", authMiddleware, shareOrder);
+router.post("/accept", authMiddleware, acceptShare);
+router.post("/decline", authMiddleware, declineShare);
 
 module.exports = router;
