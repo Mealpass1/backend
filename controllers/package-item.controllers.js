8d7a7ed5edb775f0e8dec8aaae38401a4b05aa99
Saad <byiringirosaad@gmail.com>
2022-05-18 11:45:57 +0200

    optimization

--- controllers/package-item.controllers.js
+++ controllers/package-item.controllers.js
@@ -1,69 +1,73 @@
-const Items = require("../models/package-item.model")
+const Items = require("../models/package-item.model");
 
 exports.getItems = async (req, res) => {
-    const data = {
-        package: req.params.package,
-        restaurant: req.params.restaurant
-    }
+  const data = {
+    package: req.params.package,
+    restaurant: req.params.restaurant,
+  };
 
-    try {
-        await Items.find({ package: data.package, restaurant: data.restaurant }).populate("dish").then(response => {
-            return res.json({
-                status: "success",
-                message: "package items in one restaurant",
-                data: response
-            });
-        })
-    } catch (error) {
+  try {
+    await Items.find({ package: data.package, restaurant: data.restaurant })
+      .populate("dish")
+      .then((response) => {
         return res.json({
-            status: "error",
-            message: error.message,
+          status: "success",
+          message: "package items in one restaurant",
+          data: response,
         });
-    }
-}
+      });
+  } catch (error) {
+    return res.json({
+      status: "error",
+      message: error.message,
+    });
+  }
+};
 
 exports.getItem = async (req, res) => {
-    const data = {
-        id: req.params.id,
-    }
+  const data = {
+    id: req.params.id,
+  };
 
-    try {
-
-        await Items.findById(data.id).populate("dish").then(response => {
-            return res.json({
-                status: "success",
-                message: "package item",
-                data: response
-            });
-        })
-
-    } catch (error) {
+  try {
+    await Items.findById(data.id)
+      .populate("dish")
+      .then((response) => {
         return res.json({
-            status: "error",
-            message: error.message,
+          status: "success",
+          message: "package item",
+          data: response,
         });
-    }
-}
+      });
+  } catch (error) {
+    return res.json({
+      status: "error",
+      message: error.message,
+    });
+  }
+};
 
 exports.getPackageItems = async (req, res) => {
-    const data = {
-        package: req.params.package,
-    }
+  const data = {
+    package: req.params.package,
+  };
 
-    console.log(data);
+  console.log(data);
 
-    try {
-        await Items.find({ package: data.package }).populate("dish").then(response => {
-            return res.json({
-                status: "success",
-                message: "package items",
-                data: response
-            });
-        })
-    } catch (error) {
+  try {
+    await Items.find({ package: data.package })
+      .populate("dish")
+      .then((response) => {
         return res.json({
-            status: "error",
-            message: error.message,
+          status: "success",
+          message: "package items",
+          data: response,
         });
-    }
-}
+      });
+  } catch (error) {
+    return res.json({
+      status: "error",
+      message: error.message,
+    });
+  }
+};
--- routes/package-item.routes.js
+++ routes/package-item.routes.js
@@ -1,11 +1,15 @@
 const express = require("express");
 
-const { getItems, getItem, getPackageItems } = require("../controllers/package-item.controllers")
+const {
+  getItems,
+  getItem,
+  getPackageItems,
+} = require("../controllers/package-item.controllers");
 
 const router = express.Router();
 
-router.get("/item/:id", getItem)
-router.get("/items/:package", getPackageItems)
+router.get("/item/:id", getItem);
+router.get("/items/:package", getPackageItems);
 // router.get("/:package/:restaurant", getItems)
 
 module.exports = router;
