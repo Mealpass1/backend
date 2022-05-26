22b8bf9f9f22d7a8633a87b98bc751e945bdbe61
Saad <byiringirosaad@gmail.com>
2022-05-26 11:21:15 +0200

    updates

--- services/db.service.js
+++ services/db.service.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 
 const connection = mongoose.connect(process.env.DB, {}, () => {
-  console.log("Database connected");
+  console.log("Database connected sucessfully");
 });
 
 module.exports = connection;
